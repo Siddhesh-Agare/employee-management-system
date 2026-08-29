6915a8ed96858513c822a6505bb6cb6fa463f6f1
Siddhesh Agare <siddheshagare432@gmail.com>
2026-08-29 14:30:32 +0530

    feat: create User model with schema for name, email, password, role, and status

---
+++ backend/src/models/User.model.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema(
+    {
+        name:{
+            type:String,
+            required:true
+        },
+        email:{
+            type:String,
+            required:true,
+            unique:true
+        },
+        password:{
+            type:String,
+            required:true,
+        },
+        role:{
+            type:String,
+            enum:["admin","manager","employee"],
+            required:true
+        },
+        status:{
+            type:String,
+            required:true,
+            enum:["pending","active","rejected","inactive"]
+        },
+
+
+    },
+    {
+        timestamps:true
+    }
+)
+
+const User = mongoose.model("User",userSchema);
+export default User;
