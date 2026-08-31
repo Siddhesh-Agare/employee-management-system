137b9b7bb0846ed0b05ffc3474f6fdaff58f5c8d
Siddhesh Agare <siddheshagare432@gmail.com>
2026-08-31 12:19:58 +0530

    feat: add role-based authorization middleware

---
+++ backend/src/middlewares/roleMiddleware.js
@@ -0,0 +1,16 @@
+
+export const authorize = (...roles)=>{
+
+    return (req, res, next) =>{
+
+            if(roles.includes(req.user.role)){
+                return next();
+            }
+
+            return res.status(403).json({
+                message:"You are not authenticated"
+            })
+
+
+    }
+}
