47ccef323b14bd22d26353b6424603f70ab8394c
Siddhesh Agare <siddheshagare432@gmail.com>
2026-09-04 14:28:25 +0530

    feat: add update task status functionality in employee controller and routes

--- backend/src/controllers/employeeController.js
+++ backend/src/controllers/employeeController.js
@@ -1,3 +1,4 @@
+import { response } from "express";
 import Task from "../models/Task.model.js";
 
 export const getMytask = async (req, res)=>{
@@ -18,3 +19,79 @@ export const getMytask = async (req, res)=>{
         
     }
 }
+
+export const updateTaskStatus = async (req, res) => {
+    try {
+
+        const task = await Task.findOne({
+            _id: req.params.taskId,
+            assignedTo: req.user._id
+        });
+
+        if (!task) {
+            return res.status(404).json({
+                message: "Task is not found"
+            });
+        }
+
+        // assigned → in-progress
+        if (task.status === "assigned") {
+            await Task.updateOne(
+                {
+                    _id: task._id
+                },
+                {
+                    $set: {
+                        status: "in-progress"
+                    }
+                }
+            );
+
+            return res.status(200).json({
+                message: "Task moved to in-progress successfully"
+            });
+        }
+
+        // in-progress → submitted
+        if (task.status === "in-progress") {
+
+             const { response } = req.body || {};
+
+            if (!response) {
+                return res.status(400).json({
+                    message: "Response is required before submitting the task"
+                });
+            }
+
+            await Task.updateOne(
+                {
+                    _id: task._id
+                },
+                {
+                    $set: {
+                        status: "submitted",
+                        response: response
+                    }
+                }
+            );
+
+            return res.status(200).json({
+                message: "Task submitted successfully"
+            });
+        }
+
+        // submitted/reviewed → employee cannot change
+        if (task.status === "submitted" || task.status === "reviewed") {
+            return res.status(403).json({
+                message: "You cannot change the status of this task"
+            });
+        }
+
+    } catch (error) {
+        console.log(error);
+
+        res.status(500).json({
+            message: "Error in updating status"
+        });
+    }
+};
--- backend/src/routes/employeeRoutes.js
+++ backend/src/routes/employeeRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import protect from '../middlewares/authMiddleware.js';
 import { authorize } from '../middlewares/roleMiddleware.js';
-import { getMytask } from '../controllers/employeeController.js';
+import { getMytask, updateTaskStatus } from '../controllers/employeeController.js';
 
 const router = express.Router();
 
 router.get("/tasks",protect,authorize("employee"),getMytask)
+router.patch("/tasks/:taskId/status",protect,authorize("employee"),updateTaskStatus)
 
 export default router;
