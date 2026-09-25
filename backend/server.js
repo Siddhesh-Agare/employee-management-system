9d4bbc7b996317c6a39676521fa4df2d2491a06a
Siddhesh Agare <siddheshagare432@gmail.com>
2026-09-25 20:35:32 +0530

    refactor: improve code formatting and consistency in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,35 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
-import authRouter from './src/routes/authRoutes.js';
-import userRouter from './src/routes/userRoutes.js';
-import adminRouter from './src/routes/adminRoutes.js';
-import connectDB from './src/config/db.js';
-import managerRouter from './src/routes/managerRoutes.js';
-import employeeRouter from './src/routes/employeeRoutes.js';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import authRouter from "./src/routes/authRoutes.js";
+import userRouter from "./src/routes/userRoutes.js";
+import adminRouter from "./src/routes/adminRoutes.js";
+import connectDB from "./src/config/db.js";
+import managerRouter from "./src/routes/managerRoutes.js";
+import employeeRouter from "./src/routes/employeeRoutes.js";
+
+dotenv.config();
 
 const app = express();
-dotenv.config()
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.send("response from backned");
-})
+app.get("/", (req, res) => {
+    res.send("Response from backend");
+});
 
-app.use("/api/auth",authRouter)
-app.use("/api/user",userRouter)
-app.use("/api/admin",adminRouter)
-app.use("/api/manager",managerRouter)
-app.use("/api/employee",employeeRouter)
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/admin", adminRouter);
+app.use("/api/manager", managerRouter);
+app.use("/api/employee", employeeRouter);
 
 connectDB();
 
-app.listen(5000,()=>{
-    console.log("Server is running on port 5000")
-})
+const PORT = process.env.PORT || 5000;
 
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server is running on port ${PORT}`);
+});
